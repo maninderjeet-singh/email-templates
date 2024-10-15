f8043fe7745f8b372425e4969ba1ea244da06477
Maninderjeet Singh <88441487+maninderjeet-singh@users.noreply.github.com>
2024-10-15 12:13:30 +0530

    Update index.blade.php

--- src/views/form/index.blade.php
+++ src/views/form/index.blade.php
@@ -15,6 +15,27 @@
         <h3>Create Template</h3>
         <div class="card shadow-lg mt-4">
             <div class="card-body">
+                @isset($emailTemplate)
+                    <form action="{{ route('email-templates.update', $emailTemplate->id) }}" method="post">
+                        @method('PUT')
+                    @else
+                        <form action="{{ route('email-templates.store') }}" method="post">
+                        @endisset
+
+                        <div class="mb-3 mt-3">
+                            <label for="title" class="form-label">Title:</label>
+                            <input type="text" class="form-control" id="title" placeholder="Enter title"
+                                name="title" value="{{ isset($emailTemplate) ? $emailTemplate->title : '' }}">
+                            {{-- @error('title')
+                                <span class="alert alert-danger">{{ $message }}</span>
+                            @enderror --}}
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="content" class="form-label">Content:</label>
+                            <textarea name="content" id="content" class="form-control" cols="30" rows="10">{{ isset($emailTemplate) ? $emailTemplate->content : '' }}</textarea>
+                        </div>
+                        <button type="submit" class="btn btn-dark">Submit</button>
+                    </form>
             </div>
         </div>
     </div>
