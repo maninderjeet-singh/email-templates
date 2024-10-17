0394d9c08a7a045c0fa4262dc89b214bb41d1c87
Maninderjeet Singh <88441487+maninderjeet-singh@users.noreply.github.com>
2024-10-17 16:08:26 +0530

    Update EmailTemplate.php

--- src/EmailTemplate.php
+++ src/EmailTemplate.php
@@ -3,11 +3,22 @@
 namespace Maninderjeet\EmailTemplate;
 
 use Illuminate\Support\Facades\Http;
+use Maninderjeet\EmailTemplate\Models\EmailTemplate as ModelsEmailTemplate;
 
 class EmailTemplate
 {
     public function justDoIt()
     {
         return 'Maninderjeet\EmailTemplate\EmailTemplate';
+    }
+
+    public static function templateContent($id,$parms)
+    {
+        $template = ModelsEmailTemplate::find($id);
+        $content = $template->content;
+        foreach ($parms as $key => $value) {
+            $content = str_replace("{{{$key}}}", $value, $content);
+        }
+        return $content;
     }
 }
