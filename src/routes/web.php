cb045eec835ced36e0acbe7bb4ff903620b79543
Maninderjeet Singh <88441487+maninderjeet-singh@users.noreply.github.com>
2024-10-17 18:04:36 +0530

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Maninderjeet\EmailTemplate\Controllers\EmailTemplateController;
+use Maninderjeet\EmailTemplate\Controllers\MediaController;
 
+// template routes
 Route::get('email-templates', [EmailTemplateController::class,'index'])->name('email-templates.index');
 Route::post('email-templates', [EmailTemplateController::class, 'store'])->name('email-templates.store');
 Route::get('email-templates/create', [EmailTemplateController::class, 'create'])->name('email-templates.create');
@@ -11,3 +13,5 @@ Route::get('email-templates/{id}', [EmailTemplateController::class, 'show'])->na
 Route::put('email-templates/{id}', [EmailTemplateController::class, 'update'])->name('email-templates.update');
 Route::put('email-templates/{id}/duplicate', [EmailTemplateController::class, 'duplicate'])->name('email-templates.duplicate');
 Route::delete('email-templates/{id}', [EmailTemplateController::class, 'destroy'])->name('email-templates.destroy');
+// media routes
+Route::get('medias', [MediaController::class,'index'])->name('medias.index');
